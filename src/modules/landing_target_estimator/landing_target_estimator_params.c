5895962feed6585b55bee0f5fc424a14e268d2ef
ruimscarvalho98 <ruimsc98@gmail.com>
2021-12-21 17:13:00 +0000

    Added custom frame to IR lock

--- src/modules/landing_target_estimator/landing_target_estimator_params.c
+++ src/modules/landing_target_estimator/landing_target_estimator_params.c
@@ -133,11 +133,12 @@ PARAM_DEFINE_FLOAT(LTEST_SCALE_X, 1.0f);
  */
 PARAM_DEFINE_FLOAT(LTEST_SCALE_Y, 1.0f);
 
+
 /**
  * Rotation of IRLOCK sensor relative to airframe
- * 
- * Default orientation of Yaw 270°
- * 
+ *
+ * Default orientation of Yaw 90°
+ *
  * @value 0 No rotation
  * @value 1 Yaw 45°
  * @value 2 Yaw 90°
@@ -146,10 +147,43 @@ PARAM_DEFINE_FLOAT(LTEST_SCALE_Y, 1.0f);
  * @value 5 Yaw 225°
  * @value 6 Yaw 270°
  * @value 7 Yaw 315°
- * 
+ *
  * @min -1
  * @max 40
  * @reboot_required true
  * @group Landing Target Estimator
  */
-PARAM_DEFINE_INT32(LTEST_SENS_ROT, 6);
+PARAM_DEFINE_INT32(LTEST_SENS_ROT, 2);
+
+/**
+ * X Position of IRLOCK in body frame (forward)
+ *
+ * @reboot_required true
+ * @unit m
+ * @decimal 3
+ * @group Landing Target Estimator
+ *
+ */
+PARAM_DEFINE_FLOAT(LTEST_SENS_POS_X, 0.0f);
+
+/**
+ * Y Position of IRLOCK in body frame (right)
+ *
+ * @reboot_required true
+ * @unit m
+ * @decimal 3
+ * @group Landing Target Estimator
+ *
+ */
+PARAM_DEFINE_FLOAT(LTEST_SENS_POS_Y, 0.0f);
+
+/**
+ * Z Position of IRLOCK in body frame (downward)
+ *
+ * @reboot_required true
+ * @unit m
+ * @decimal 3
+ * @group Landing Target Estimator
+ *
+ */
+PARAM_DEFINE_FLOAT(LTEST_SENS_POS_Z, 0.0f);
